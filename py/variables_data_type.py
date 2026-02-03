1022b3e3e7120bc27bd5b39ff4faff1734043f18
joypetanimodern <izzatki42@gmail.com>
2026-02-03 12:49:38 +0800

    first commit

---
+++ py/variables_data_type.py
@@ -0,0 +1,26 @@
+# # variable data types
+
+name = "bob"       # String
+age = 10           # Integer
+weight = 20.5      # Float
+is_female = False  # Boolean
+her_name = "alan"
+
+print(name)
+print (type(name))
+print(age)
+print(type(age))
+print(weight)
+print(type(weight))
+print(her_name)
+print(type(her_name))
+print (is_female)
+print(type(is_female))
+
+
+
+
+
+c = celcius = 25 
+f = c*9/5+32
+print (f)
